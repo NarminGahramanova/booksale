cb1ecb7eeae63eb1306bc9ce37bbb6a6bf9415cb
Narmin <gahramanovanarmin02@gmail.com>
2026-07-01 12:08:42 +0400

    fix: add bloc and meta as direct dependencies

--- lib/domain/usecases/get_cart_usecase.dart
+++ lib/domain/usecases/get_cart_usecase.dart
@@ -1,12 +1,13 @@
 
 
+import '../entities/cart.dart';
 import '../repositories/cart_repo.dart';
 
 class GetCartUsecase {
   final CartRepo  repository;
 
   GetCartUsecase({required this.repository});
-   Future<void>call(){
+   Future<Cart>call(){
      return repository.getCart();
 
    }
